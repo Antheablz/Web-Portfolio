7603b1a55c2794b1495b7c5a6eea2abcb0d2aa02
Antheablz <agblais@icloud.com>
2024-04-21 10:58:15 -0700

    btn effects and background blur added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
     const menu = document.getElementById("mobile-dropdown");
     const icon = document.getElementById("mobile-hamburger");
+    const body = document.getElementById("temp");
 
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    body.classList.toggle("blur")
 }
