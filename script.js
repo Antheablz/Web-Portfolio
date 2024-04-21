a404b184bced115e9d1b3fd9f52691b73b5467d5
Antheablz <agblais@icloud.com>
2024-04-21 12:21:41 -0700

    started the about me section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleMenu() {
     const menu = document.getElementById("mobile-dropdown");
     const icon = document.getElementById("mobile-hamburger");
-    const body = document.getElementById("temp");
+    const body = document.getElementById("blur-container");
 
     menu.classList.toggle("open");
     icon.classList.toggle("open");
