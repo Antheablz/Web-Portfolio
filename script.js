567790eb3b4ce949f934bd96c6d2a2b3f0d608eb
Antheablz <agblais@icloud.com>
2024-04-21 22:35:39 -0700

    starting work experience section, added dialog

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function toggleMenu() {
     icon.classList.toggle("open");
     body.classList.toggle("blur")
 }
+
+function openDialog() {
+    let dialog = document.getElementById("work-dialog");
+    let body = document.getElementById("blur-container");
+
+    dialog.showModal()
+    body.classList.toggle("blur")
+    console.log("FUCK YA DIALOG");
+}
+
+function closeDialog(elem_id) {
+    let dialog = document.getElementById(elem_id);
+    let body = document.getElementById("blur-container");
+    
+    body.classList.toggle("blur")
+    dialog.close();
+}
